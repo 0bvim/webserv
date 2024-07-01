f41024c3b7c4786f13b40547fa2eeb062933b713
Bruno Moretti <bmoretti@c1r9p1.42sp.org.br>
2024-07-01 09:40:02 -0300

    returning tabs identation

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 10:40:41 by bmoretti          #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:51 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/07/01 09:38:53 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@
 
 class Server {
 public:
-  Server();
-  ~Server();
+	Server();
+	~Server();
 
 private:
-  int _serverSocket;
-  struct sockaddr_in _address;
-  std::vector<Client *> _clients;
+	int						_serverSocket;
+	struct sockaddr_in		_address;
+	std::vector<Client *>	_clients;
 
-  void _createServerSocket(void);
-  void _bindServer(void);
-  void _listen(void);
-  void _acceptClient(void);
+	void	_createServerSocket(void);
+	void	_bindServer(void);
+	void	_listen(void);
+	void	_acceptClient(void);
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 10:40:35 by bmoretti          #+#    #+#             */
-/*   Updated: 2024/06/29 20:04:54 by vde-frei         ###   ########.fr       */
+/*   Updated: 2024/07/01 09:39:33 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,47 @@
 // PUBLIC FUNCTIONS
 
 Server::Server() {
-  this->_address.sin_family = AF_INET;
-  this->_address.sin_addr.s_addr = INADDR_ANY;
-  this->_address.sin_port = htons(PORT);
-  this->_createServerSocket();
-  this->_bindServer();
-  this->_listen();
+	this->_address.sin_family = AF_INET;
+	this->_address.sin_addr.s_addr = INADDR_ANY;
+	this->_address.sin_port = htons(PORT);
+	this->_createServerSocket();
+	this->_bindServer();
+	this->_listen();
 }
 
 Server::~Server() {
-  for (std::vector<Client *>::iterator it = this->_clients.begin();
-       it != this->_clients.end(); it++) {
-    delete *it;
-  }
-  close(this->_serverSocket);
+	for (std::vector<Client *>::iterator it = this->_clients.begin();
+		it != this->_clients.end(); it++) {
+	delete *it;
+	}
+	close(this->_serverSocket);
 }
 
 // PRIVATE FUNCTIONS
 
 void Server::_createServerSocket(void) {
-  this->_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-  if (this->_serverSocket == -1) {
-    throw std::runtime_error("Error creating the server socket");
-  }
+	this->_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (this->_serverSocket == -1) {
+	throw std::runtime_error("Error creating the server socket");
+	}
 }
 
 void Server::_bindServer(void) {
-  if (bind(this->_serverSocket, (struct sockaddr *)&this->_address,
-           sizeof(this->_address)) == -1) {
-    throw std::runtime_error("Error binding the server");
-  }
+	if (bind(this->_serverSocket, (struct sockaddr *)&this->_address,
+			sizeof(this->_address)) == -1) {
+	throw std::runtime_error("Error binding the server");
+	}
 }
 
 void Server::_listen(void) {
-  if (listen(this->_serverSocket, MAX_CONNECTIONS) == -1) {
-    throw std::runtime_error("Error listening the server");
-  }
+	if (listen(this->_serverSocket, MAX_CONNECTIONS) == -1) {
+	throw std::runtime_error("Error listening the server");
+	}
 }
 
 void Server::_acceptClient(void) {
-  Client *client = new Client();
+	Client *client = new Client();
 
-  client->connectToServer(this->_serverSocket);
-  this->_clients.push_back(client);
+	client->connectToServer(this->_serverSocket);
+	this->_clients.push_back(client);
 }
