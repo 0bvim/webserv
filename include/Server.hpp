6ecfe10e4e31452e39f6385a712d363c6f988c24
Vinicius De Freitas Pereira <vde-frei@c1r10p1.42sp.org.br>
2024-07-06 22:16:36 -0300

    feat: added new file and removed old ones

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,40 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 10:40:41 by bmoretti          #+#    #+#             */
-/*   Updated: 2024/07/02 11:49:50 by bmoretti         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#pragma once
 
-#ifndef SERVER_HPP
-#define SERVER_HPP
-
-#include "Client.hpp"
-#include "common.hpp"
-
-class Server {
+class Server
+{
 public:
-	Server();
-	~Server();
+
+protected:
 
 private:
-	int							_serverSocket;
-	struct sockaddr_in			_address;
-	std::vector<Client *>		_clients;
-
-// Server initialization
-	void	_createServerSocket(void);
-	void	_bindServer(void);
-	void	_listen(void);
-	void	_polling(void);
-
-// Client management
-	void	_acceptClient(void);
+  const char* m_ipAddress; //Ip address server will run on
+  int m_port; // Port # for the web service
+  int m_socket; // Internal FD for the listening socket
 
 };
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,6 @@
 # include "Server.hpp"
 
-bool g_sigint = false;
-
-void	handle_signal(int c)
-{
-	(void)c;
-	g_sigint = true;
-	OUTNL(RED("\r[HALT]: STOPPING THE SERVER... \n"));
-}
-
 int	main(void)
 {
-	signal(SIGPIPE, SIG_IGN);
-	signal(SIGINT, handle_signal);
-
-	try {
-		Server serv;
-	}
-
-	catch (std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
 	return 0;
 }
