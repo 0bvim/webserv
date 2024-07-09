604381cf0a265596e49792a18b5da7c40aa1766c
nivicius <niviciusdev@gmail.com>
2024-07-09 02:53:36 -0300

    incremented tests in google suite

--- tests/testServer.cpp
+++ tests/testServer.cpp
@@ -131,3 +131,118 @@ TEST_F(ServerTest, HandlesInvalidClientConnections) {
 
     close(sock);
 }
+
+TEST_F(ServerTest, HandlesServerShutdown) {
+    serverThread.detach();  // Detach the thread to simulate shutdown
+    delete server;  // Delete the server instance to simulate shutdown
+
+    // Attempt to connect after shutdown
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(sock, -1);
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(PORT);
+
+    int result = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    ASSERT_EQ(result, -1);  // Connection should fail
+
+    close(sock);
+}
+
+TEST_F(ServerTest, NonBlockingBehavior) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(sock, -1);
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(PORT);
+
+    int result = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    ASSERT_NE(result, -1);
+
+    const char* message = "Hello Server";
+    send(sock, message, strlen(message), 0);
+
+    char buffer[1024];
+    int flags = fcntl(sock, F_GETFL, 0);
+    ASSERT_NE(flags, -1);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+
+    int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_received == -1) {
+        ASSERT_EQ(errno, EAGAIN);
+    } else {
+        ASSERT_GT(bytes_received, 0);
+    }
+    close(sock);
+}
+
+// tests/test_Server.cpp (continuation)
+TEST_F(ServerTest, HandlesLargePayload) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(sock, -1);
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(8080);
+
+    int result = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    ASSERT_NE(result, -1);
+
+    std::string large_message(10000, 'A');
+    send(sock, large_message.c_str(), large_message.size(), 0);
+
+    char buffer[1024];
+    int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    ASSERT_GT(bytes_received, 0);
+
+    buffer[bytes_received] = '\0';
+    std::string response(buffer);
+    ASSERT_EQ(response, "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+
+    close(sock);
+}
+
+TEST_F(ServerTest, EdgeTriggeredEvents) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(sock, -1);
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(8080);
+
+    int result = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    ASSERT_NE(result, -1);
+
+    const char* message = "Hello Server";
+    send(sock, message, strlen(message), 0);
+
+    char buffer[1024];
+    int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    ASSERT_GT(bytes_received, 0);
+
+    buffer[bytes_received] = '\0';
+    std::string response(buffer);
+    ASSERT_EQ(response, "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+
+    send(sock, message, strlen(message), 0);
+    bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    ASSERT_GT(bytes_received, 0);
+
+    buffer[bytes_received] = '\0';
+    response = std::string(buffer);
+    ASSERT_EQ(response, "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+
+    close(sock);
+}
+
+
