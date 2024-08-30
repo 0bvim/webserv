95691c51ea195e6b8757e08f009fe62b3b2eb9e1
nivicius <niviciusdev@gmail.com>
2024-08-30 17:18:42 -0300

    fix: move some includes to server

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sys/epoll.h>
 
 class Configuration;
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
 #include <stdexcept>
-#include <vector>
-#include <string>
-#include <map>
-#include <fstream>
-#include <sstream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <cstring>
-#include <sys/epoll.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include "Server/Server.hpp"
 #include "Configuration/Configuration.hpp"
-
-
 
 // Main function
 int main()
